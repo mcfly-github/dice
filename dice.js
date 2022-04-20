5424f543450195b5d5c35a19c747165e63703855
mcfly-github <92212396+mcfly-github@users.noreply.github.com>
2022-04-20 15:30:02 +0200

    v1.0

---
+++ dice.js
@@ -0,0 +1,143 @@
+
+let data = [0,1,2,3,4,5];
+let computerRoll = data[Math.floor(Math.random()*data.length)];
+let playerRoll = data[Math.floor(Math.random()*data.length)];
+let images = ["pics/inverted-dice-1.svg",
+                "pics/inverted-dice-2.svg",
+                "pics/inverted-dice-3.svg",
+                "pics/inverted-dice-4.svg",
+                "pics/inverted-dice-5.svg",
+                "pics/inverted-dice-6.svg"
+                ];
+let computerWinCounter = Number(localStorage.getItem("computerwin"));
+let playerWinCounter = Number(localStorage.getItem("playerwin"));
+let tieCounter = Number(localStorage.getItem("tie"));
+
+function showCounters() {
+
+    document.getElementById("computer-win-counter").innerHTML = computerWinCounter;
+    document.getElementById("player-win-counter").innerHTML = playerWinCounter;
+    document.getElementById("tie-counter").innerHTML = tieCounter;
+
+}
+
+
+function reloadPage() {
+
+    location.reload()
+
+}
+
+
+function buttonCheck() {
+
+    let counter = 0;
+    document.getElementById("roll-button").onclick = () => {
+    counter++
+        if (counter == 1) {
+
+            reloadPage()
+
+        }
+    }
+}
+
+
+function computer() {
+
+    
+    return computerRoll;
+}
+
+
+function player() {
+
+    
+    return playerRoll;
+}
+
+
+function winner() {
+
+    if (computerRoll > playerRoll) {
+        document.getElementById("winner-is").innerHTML = "Computer";
+        computerWinCounter += 1
+        localStorage.setItem("computerwin", computerWinCounter);
+        document.getElementById("computer-win-counter").innerHTML = localStorage.getItem("computerwin");
+    } else if (computerRoll < playerRoll) {
+        document.getElementById("winner-is").innerHTML = "You";
+        playerWinCounter += 1
+        localStorage.setItem("playerwin", playerWinCounter);
+        document.getElementById("player-win-counter").innerHTML = localStorage.getItem("playerwin");
+    } else {
+        document.getElementById("winner-is").innerHTML = "It's a tie";
+        tieCounter += 1
+        localStorage.setItem("tie", tieCounter);
+        document.getElementById("tie-counter").innerHTML = localStorage.getItem("tie");
+    }
+}
+
+
+function computerDice() {
+
+    let img = document.createElement("img");
+    img.src = images[computerRoll];
+    img.setAttribute("width", "100px");
+    img.setAttribute("height", "100px");
+    document.getElementById("computer-dice-pic").appendChild(img);
+}
+
+
+function playerDice() {
+
+    let checkElement = !!document.getElementById("player-dice");
+
+    if (checkElement == true) {
+
+        reloadPage()
+
+    } else {
+
+        let img = document.createElement("img");
+        img.src = images[playerRoll];
+        img.setAttribute("id", "player-dice");
+        img.setAttribute("width", "100px");
+        img.setAttribute("height", "100px");
+        document.getElementById("player-dice-pic").appendChild(img);
+    }
+}
+
+
+function textChange() {
+
+    document.getElementById("roll-button").innerHTML = "Play again";
+    
+}
+
+
+function setLocalStorage() {
+
+    if (localStorage.getItem("computerwin") == null) {
+        localStorage.setItem("computerwin", 0);
+    }
+
+    if (localStorage.getItem("playerwin") == null) {
+        localStorage.setItem("playerwin", 0);
+    }
+
+    if (localStorage.getItem("tie") == null) {
+        localStorage.setItem("tie", 0);
+    }
+    
+}
+
+
+function logout() {
+
+    localStorage.removeItem("computerwin")
+    localStorage.removeItem("playerwin")
+    localStorage.removeItem("tie")
+    location.href = "welcome.html"
+    
+
+}
---
+++ welcome.js
@@ -0,0 +1,49 @@
+function storeCredentials() {
+
+    let username = document.getElementById("reg-username").value;
+    let password = document.getElementById("reg-password").value;
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    statusCheck()
+    document.getElementById("sign-result").innerHTML = "Registration is succesful, please sign in!";
+    document.getElementById("login").hidden = false;
+    
+
+}
+
+function checkCredentials() {
+
+    if (checkUsername() && checkPassword() == true) {
+        console.log("Login OK")
+        location.href = "dice.html"
+    } else {
+        document.getElementById("sign-result").innerHTML = "Wrong username and/or password.";
+    }
+}
+
+function checkUsername() {
+
+    if (document.getElementById("username").value === localStorage.getItem("username")) {
+        return true;
+    }
+}
+
+function checkPassword() {
+
+    if (document.getElementById("password").value === localStorage.getItem("password")) {
+        return true;
+    }
+}
+
+
+function statusCheck() {
+
+    if (localStorage.getItem("username") != null) {
+        document.getElementById("title").innerHTML = "Login"
+        document.getElementById("register").hidden = true;
+    } else {
+        document.getElementById("login").hidden = true;
+
+    }
+    
+}
